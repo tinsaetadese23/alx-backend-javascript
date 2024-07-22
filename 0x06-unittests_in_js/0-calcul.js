fff5c4ff50f3a2b4d24ef5e9156f61fd622cc1c7
Tinsae Tadese <tinsaetadese23@gmail.com>
2024-07-22 00:16:24 -0700

    Basic test with Mocha and Node assertion library

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,9 +1,5 @@
-module.exports = function calculateNumber(a, b = 0) {
-  const aNum = Number(a);
-  const bNum = Number(b);
-  
-  if (Number.isNaN(aNum) || Number.isNaN(bNum))
-    throw TypeError('Parameters must be numbers');
-  
-    return Math.round(aNum) + Math.round(bNum);
-  };
+function calculateNumber(a, b) {
+    return Math.round(a) + Math.round(b);
+};
+
+module.exports = calculateNumber;
